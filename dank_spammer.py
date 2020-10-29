3a4482b23df5e30a22f6b39b1114a2151e96df65
callmelurker <61448797+callmelurker@users.noreply.github.com>
2020-10-28 20:13:33 -0400

    Add files via upload

---
+++ dank_spammer.py
@@ -0,0 +1,9 @@
+import pyautogui
+from time import sleep
+
+for i in range(20):
+    pyautogui.click(477,993)
+    sleep(0.1)
+    pyautogui.typewrite("pls beg")
+    pyautogui.press("enter")
+    sleep(45) 
